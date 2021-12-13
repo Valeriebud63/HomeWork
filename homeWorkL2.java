6bd0ca41fa68483a7db9b8e7181f49f85fae76e4
Valeriebud63 <81657323+Valeriebud63@users.noreply.github.com>
2021-12-13 11:29:57 +0300

    Add files via upload

---
+++ homeWorkL2.java
@@ -0,0 +1,38 @@
+package Lesson2;
+
+public class homeWorkL2 {
+    public static void main(String[] args) {
+    }
+   // 1.
+//   public static  boolean checkDigits(int a, int b) {
+//       a = 3;
+//       b = 13;
+//       int sum = a + b;
+//       return (sum >= 10) && (sum <= 20);
+//
+//   }
+   // 2.
+//    public static void isPositiveOrNegative(int p) {
+//        if (p >= 0) {
+//            System.out.println(p + "положительное" );
+//        } else {
+//            System.out.println(p + "отрицательное" );
+//        }
+//    }
+   // 3.
+//       public static boolean isPositive(int v) {
+//            if (v <= 0)  {
+//            return true;
+//                }
+//              {  return false;
+//              }
+//           }
+    // 4.
+//   public static void printStringNDigit(String value, int count) {
+//       for (int i = 1; i <= count; i++) {
+//           System.out.println("Текст № " + i + " : " + value);
+//       }
+//   }
+//
+//}
+
